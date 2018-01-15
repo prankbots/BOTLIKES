689dc434e213a1f73196bf374681fcd6a0b666ab
PRANKBOTS <33993971+Aprank@users.noreply.github.com>
2018-01-15 16:18:36 +0700

    Update channel.py

--- STN/Api/channel.py
+++ STN/Api/channel.py
@@ -19,7 +19,7 @@ class Channel:
     channel_query_path = "/CH4"
 
     UA = "Line/7.18.0"
-    LA = "CHROMOS\t7.18.0\tChrome_OS\t10"
+    LA = "IOSIPAD\t7.18.0\tiPhone OS\t11.12.1"
 
     authToken = None
     mid = None
